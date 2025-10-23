2f9c4264a36f65229e7e324dcf59428cf1fcaeb1
UriAcelga <urigomez2002@gmail.com>
2025-10-23 13:38:46 -0300

    corregida selección por teclado. Añadido disparador de evento dispatch para interacción entre componentes

--- resources/views/components/dropdown-interno.blade.php
+++ resources/views/components/dropdown-interno.blade.php
@@ -40,58 +40,42 @@
                     this.open = false
                 } else if (event.key === 'Escape') {
                     this.close()
-                } else if (event.key === 'Enter' || event.key === ' ') {
-                    event.preventDefault()
-                    this.selectOption(this.$refs.listbox.children[this.focused_index].dataset.value,
-                        this.$refs.listbox.children[this.focused_index].dataset.name )    
                 }
             } else {
-                if (event.key === 'ArrowDown' || event.key === 'ArrowUp' ) {
+                if (event.key === 'ArrowDown' ||
+                event.key === 'ArrowUp' ) {
                     this.toggle()
                 }
             }
         }
-    }" x-on:keydown.stop="navigate($event)" x-id="['dropdown-panel']" class="relative w-full">
-        <!-- Hidden Input: Dispatch manda datos en el evento para que otros componentes lo reconozcan -->
-        <input type="hidden"
-            name="{{ $field_name }}"
-            x-bind:value="input_value"
-            x-effect="$dispatch('input-change', {field: $el.name, value: $el.value})">
-        <!-- Boton -->
+    }" x-on:keydown.stop="navigate($event)" x-id="['dropdown-button']" class="relative w-full">
+        <!-- Hidden Input -->
+        <input type="hidden" name="{{ $field_name }}" x-bind:value="input_value">
+        <!-- Button -->
         <span class="block mb-2 text-sm font-medium text-gray-900">Facultad</span>
         <button type="button" tabindex=0 
             x-ref="button"
             x-on:click="toggle()" 
             :aria-expanded="open"
-            :aria-controls="$id('dropdown-panel')" aria-haspopup="listbox"
+            :aria-controls="$id('dropdown-button')" aria-haspopup="listbox"
             class="relative flex items-center whitespace-nowrap justify-between gap-2 w-full h-12 px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
             >
-            <span x-text="select_prompt"></span>
+            <span x-text="focused_index"></span>
 
             <img src="{{ asset('icons/dropdown-arrow.svg') }}" class="w-4">
 
         </button>
 
         <!-- Panel -->
-        <ul role="listbox" 
-            x-ref="listbox"
-            x-show="open"
-            x-cloak
-            x-on:click.outside="close()"
-            :id="$id('dropdown-panel')"
-
+        <ul role="listbox" x-show="open" x-cloak x-on:click.outside="close()" :id="$id('dropdown-button')"
             class="absolute left-0 w-full rounded-lg shadow-sm mt-2 z-10 origin-top-left bg-white outline-none border border-gray-200">
-            <!-- Opciones -->
             @foreach ($facultades as $facultad)
-                <li role="option" 
-                    data-index="{{ $loop->index }}"
-                    data-value="{{ $facultad->codigo }}"
-                    data-name="{{ $facultad->nombre }}"
+                <li role="option" data-index="{{ $loop->index }}"
                     x-on:click.prevent="selectOption('{{ $facultad->codigo }}','{{ $facultad->nombre }}')"
                     class="p-2.5 w-full flex items-center rounded-md transition-colors text-left text-gray-800 hover:bg-blue-100 focus-visible:bg-blue-100 disabled:opacity-50 disabled:cursor-not-allowed border-b border-gray-200"
                     :class="$el.dataset.index == focused_index ? 'ring-3 ring-blue-700' : ''">
                     <span
-                        class="w-16 text-center bg-blue-900 text-white text-xs font-bold me-2    py-0.5 rounded-sm">{{ $facultad->codigo }}</span>{{ $facultad->nombre }}
+                        class="w-16 text-center bg-blue-900 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm">{{ $facultad->codigo }}</span>{{ $facultad->nombre }}
                 </li>
             @endforeach
         </ul>
