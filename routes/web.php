ab65ac13c3094a8d32845c0441fddf2d8885fdfd
UriAcelga <urigomez2002@gmail.com>
2025-11-15 12:48:18 -0300

    Creado middleware prevent-back para impedir que usuarios con sesión cerrada vuelvan a páginas que requieran autentificación

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth');
-Route::get('/login', [HomeController::class, 'login'])->name('login');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+Route::middleware(['auth', 'prevent-back'])->group(function() {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/solicitud', [HomeController::class, 'solicitudes'])->name('solicitud');
+});
 
+Route::get('/login', [HomeController::class, 'login'])->name('login');
 Route::get('/registro', [HomeController::class, 'registro'])->name('registro');
-Route::get('/solicitud', [HomeController::class, 'solicitudes'])->name('solicitud')->middleware('auth');
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('register', [AuthController::class, 'register'])->name('register');
