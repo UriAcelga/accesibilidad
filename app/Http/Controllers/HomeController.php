ab7b0ae71eb9b0cd71b55c08712b414fb2ab7f32
UriAcelga <urigomez2002@gmail.com>
2025-11-27 11:56:14 -0300

    Nueva dependencia ligera spatie/query-builder para sintaxis más limpia a la hora de filtrar y ordenar consultas con paginación. Creado EstudianteService con métodos para retornar los estudiantes.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\EstudianteService;
 use Illuminate\Http\Request;
 use App\Services\FacultadService;
-use App\Services\WordService;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     protected $facultadService;
+    protected $estudianteService;
 
-    public function __construct(FacultadService $data)
+    public function __construct(FacultadService $facultad, EstudianteService $estudiante)
     {
-        $this->facultadService = $data;
+        $this->facultadService = $facultad;
+        $this->estudianteService = $estudiante;
     }
 
-    public function index() {
-        return view('index');
+    public function index(Request $request) {
+        if(Auth::user()->is_admin) {
+            $registrosEstudiante = $this->estudianteService->fetchTableCols($request);
+        } else {
+            $registrosEstudiante = $this->estudianteService->fetchTableColsForGuest($request);
+        }
+        $registrosFacultad = $this->facultadService->fetch();
+        /*
+        $sort = request('sort');
+        $ariaSort = 'none';
+        if($sort) {
+            $ariaSort = $sort[0] === '-' ? 'descending' : 'ascending';
+        }
+
+        dsp en th:
+        <th scope="col" aria-sort="{{ $ariaSort }}"> 
+
+        //asc
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 3V21M7 3L11 7M7 3L3 7M14 3H15M14 9H17M14 15H19M14 21H21" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+
+        //desc
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 3V21M7 21L3 17M7 21L11 17M14 3H21M14 9H19M14 15H17M14 21H15" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+
+        */
+        return view('index', [
+            'facultades' => $registrosFacultad,
+            'estudiantes' => $registrosEstudiante,
+        ]);
     }
 
     public function solicitudes() {
@@ -27,6 +55,7 @@ class HomeController extends Controller
         $word = new WordService();
         $word->cerrarFicha($ruta, 'rehabilitación terminada, ya no requiere asistencia ni monitoreo.');
         dd(WordService::getDocMetadata($ruta));*/
+
         return view('solicitud', [
             'facultades' => $data
         ]);
---
+++ app/Services/EstudianteService.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Services;
+use App\Models\Estudiante;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class EstudianteService{
+    /**
+     * Retorna todas las facultades con sus respectivos departamentos y carreras. (Tanto id como nombre)
+     */
+    public function fetchAll(): Collection
+    {
+        return Estudiante::with('facultad', 'carrera', 'departamento')->get();
+    }
+
+    public function fetchTableCols(Request $request): Collection {
+        return QueryBuilder::for(Estudiante::class, $request)
+            ->allowedFields([
+                'id',
+                'apellido',
+                'nombre',
+                'email',
+                'telefono',
+                'cud',
+                'created_at',
+                'facultad_codigo',
+                'carrera_id'
+            ])
+            ->allowedFilters([
+                //Búsqueda por texto por nombre o CUD
+                AllowedFilter::callback('buscar', function($query, $valor) {
+                    $query->where(function ($query) use ($valor) {
+                        $query->where('apellido', 'like', "%{$valor}%")
+                            ->orWhere('nombre', 'like', "%{$valor}%")
+                            ->orWhere('cud', 'like', "%{$valor}%");
+                    });
+                }),
+
+                //Filtros por dropdown para facultad y carrera (departamento ??)
+                AllowedFilter::exact('facultad.codigo'),
+                AllowedFilter::exact('carrera.id'),
+
+                //Rangos de fecha
+                AllowedFilter::callback('creado_antes_de', function ($query, $valor) {
+                    $query->where('created_at', '<=', $valor);
+                }),
+                AllowedFilter::callback('creado_despues_de', function ($query, $valor) {
+                    $query->where('created_at', '>=', $valor);
+                })
+            ])
+            ->allowedSorts([
+                'apellido',
+                'nombre',
+                'email',
+                'telefono',
+                'cud',
+                'created_at',
+                'facultad_codigo',
+                'carrera.nombre'
+            ])
+            ->allowedIncludes(['facultad', 'carrera'])
+            ->defaultSort('-created_at')
+            ->with(['facultad', 'carrera'])
+            ->paginate(20)
+            ->appends(request()->query());
+            
+    }
+    /* Sin información de contacto (teléfono e email) */
+    public function fetchTableColsForGuest(Request $request): Collection {
+        return QueryBuilder::for(Estudiante::class, $request)
+            ->allowedFields([
+                'id',
+                'apellido',
+                'nombre',
+                'cud',
+                'created_at',
+                'facultad_codigo',
+                'carrera_id'
+            ])
+            ->allowedFilters([
+                //Búsqueda por texto por nombre o CUD
+                AllowedFilter::callback('buscar', function($query, $valor) {
+                    $query->where(function ($query) use ($valor) {
+                        $query->where('apellido', 'like', "%{$valor}%")
+                            ->orWhere('nombre', 'like', "%{$valor}%")
+                            ->orWhere('cud', 'like', "%{$valor}%");
+                    });
+                }),
+
+                //Filtros por dropdown para facultad y carrera (departamento ??)
+                AllowedFilter::exact('facultad_codigo'),
+                AllowedFilter::exact('carrera.id'),
+
+                //Rangos de fecha
+                AllowedFilter::callback('creado_antes_de', function ($query, $valor) {
+                    $query->where('created_at', '<=', $valor);
+                }),
+                AllowedFilter::callback('creado_despues_de', function ($query, $valor) {
+                    $query->where('created_at', '>=', $valor);
+                })
+            ])
+            ->allowedSorts([
+                'apellido',
+                'nombre',
+                'cud',
+                'created_at',
+                'facultad_codigo',
+                'carrera.nombre'
+            ])
+            ->allowedIncludes(['facultad', 'carrera'])
+            ->defaultSort('-created_at')
+            ->with(['facultad', 'carrera'])
+            ->paginate(20)
+            ->appends(request()->query());
+    }
+}
