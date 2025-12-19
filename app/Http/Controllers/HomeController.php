850e6df12d1ff5a995250d587184d870ad83e762
UriAcelga <urigomez2002@gmail.com>
2025-12-19 01:00:30 -0300

    limpieza de comentarios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Estudiante;
 use App\Services\EstudianteService;
 use Illuminate\Http\Request;
 use App\Services\FacultadService;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -21,9 +19,6 @@ class HomeController extends Controller
     }
 
     public function index(Request $request) {
-        // if($request['fecha_hasta'] !== null) {
-        //     dd(Carbon::parse($request['fecha_hasta'])->endOfDay()->toDateTimeString());
-        // }
         if(Auth::user()->is_admin) {
             $registrosEstudiante = $this->estudianteService->fetchTableCols($request);
         } else {
@@ -39,12 +34,6 @@ class HomeController extends Controller
 
     public function solicitudes() {
         $data = $this->facultadService->fetch();
-
-        /*$ruta = Storage::disk('local')->path('seguimientos') . '/' . 'gomez_uriel_fernando_1111111111_2025-11-06_14-26-01.docx';
-        $word = new WordService();
-        $word->cerrarFicha($ruta, 'rehabilitación terminada, ya no requiere asistencia ni monitoreo.');
-        dd(WordService::getDocMetadata($ruta));*/
-
         return view('solicitud', [
             'facultades' => $data
         ]);
